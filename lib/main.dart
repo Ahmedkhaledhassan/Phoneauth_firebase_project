ad37c41a70a4367df0952561c01270672e8d622d
ahmedkhaledhasan40@gmail.com <ahmedkhaledhasan40@gmail.com>
2022-12-21 11:21:41 -0800

    install firebase core,auth,cloud,firebase storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_pro/screens/regester_screen.dart';
 import 'package:firebase_pro/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
