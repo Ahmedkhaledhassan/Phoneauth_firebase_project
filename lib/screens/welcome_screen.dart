8cd85bc64ca20833864938929dd5dbb30c06465a
ahmedkhaledhasan40@gmail.com <ahmedkhaledhasan40@gmail.com>
2022-12-20 22:58:39 -0800

    created a widget for Custom button

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,66 @@
+import 'package:firebase_pro/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "sign1.jpg",
+                  height: 300,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Let's get started",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "Never a better time than now to start.",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black38,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                //customer_button
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: CustomButton(
+                    onPressed: () {},
+                    text: "Get started",
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
