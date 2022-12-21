6953da3d7fcf2e1b88122bc59f1a99e6751b15e2
ahmedkhaledhasan40@gmail.com <ahmedkhaledhasan40@gmail.com>
2022-12-21 12:01:41 -0800

    downloaded provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_pro/screens/regester_screen.dart';
 import 'package:firebase_pro/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_pro/screens/welcome_screen.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RegisterScreen(),
+      home: WelcomeScreen(),
       title: "flutter_PhoneAuth",
     );
   }
